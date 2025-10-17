b7e6d8cac8f55527c2597858d36dc46bb5527887
Anmitchell <avarynmitchell@gmail.com>
2025-10-17 06:41:35 -0400

    Added packages, changed configurations for package.json to allow intellisence, type checking, and compilation, added environment variables, and ignore file to prevent github from displaying sensitive information

---
+++ server/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config(); // Load environment variables from .env file.
+
+const app = express(); // Create an Express application.
+
+app.use(express.json()); // Parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(cors()); // Allows requests from client-side to server-side
+
+const PORT = process.env.PORT || 3000; // Get port from environment variables or use 3000 as default.
+
+// output a message to the console when the server is running
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
